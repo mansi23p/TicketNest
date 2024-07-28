4967d4096dcbdc4cb6d1b08863651aa26facaa63
Mansi Patil <mansi@example.com>
2024-07-28 16:28:05 +0530

    first commit

---
+++ front-end/src/data.js
@@ -0,0 +1,6 @@
+let moviesList = ['Animal', 'Munja', 'Kalki', 'Houseful 4', 'ye javani he divani','Om shanti Om','3-Idiot','Dhoom 2'];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
